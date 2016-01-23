0279d6d4b0c08f1f858a931a8efd16c46968e464
Smedira <DSmedira@gmail.com>
2016-01-23 00:16:17 -0500

    json

--- markov/sim.go
+++ markov/sim.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	"math/rand"
+
+	"encoding/json"
 )
 
-func s (numTrains int, safetyTime int, rideTime int, minTransTime int, avgTransTime int) int{
-	cc:=1
+func s (numTrains int, safetyTime int, rideTime int, minTransTime int, avgTransTime int) []int{
+	d := make([]int,0)
+	
 	trains := make([]int,numTrains)
 	c := 0
 	avg := float64(avgTransTime - minTransTime)
@@ -16,7 +19,7 @@ func s (numTrains int, safetyTime int, rideTime int, minTransTime int, avgTransT
 	t = dep
 	trains[c] = dep + rideTime
 	c = (c + 1) % numTrains
-	fmt.Println(dep)
+	d = append(d, dep)
 	for t < 43200 { //in seconds
 		if trains[c] > t {
 			t = trains[c]
@@ -26,15 +29,18 @@ func s (numTrains int, safetyTime int, rideTime int, minTransTime int, avgTransT
 			t = dep + safetyTime
 		}
 		dep = t
-		fmt.Println(dep)
-		cc += 1
+		d = append(d,dep)
 	}
-	return cc
+	fmt.Println(d)
+	return d
 }
 
 func main(){
-	a := s(3,30,50,20, 27)
+	a,_ := json.Marshal(s(3,30,50,20, 27))
 	fmt.Println(a)
+	var dat []int
+	json.Unmarshal(a, &dat)
+	fmt.Println(dat)
 }
 		
 	
